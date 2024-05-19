26bc109edeb20a3266e32659237dd6faacef9d55
cesar <cesariofico1@gmail.com>
2024-05-19 12:30:20 +0900

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function goToAbout() {
+    window.location.href = "about.html";
+}
+function goToweb() {
+    window.location.href = "web.html";
+}
+
+function playSound() {
+    var audio = new Audio('select-sound-121244.mp3'); // Ganti dengan path file suara Anda
+    audio.play();
+}
